a79826ca47d68cd10075b92daa21e601afbb5e89
coderjs-lab <s.harmanjot2007@gmail.com>
2020-07-26 13:35:16 +0530

    UI

--- components/AppDrawerNavigation.js
+++ components/AppDrawerNavigation.js
@@ -8,18 +8,35 @@ import {bottomTabNavigator} from '../components/bottomTabNavigator';
 import MyBartersScreen from '../screens/MyBartersScreen';
 import Notifications from '../screens/Notifications';
 
+import {Icon} from 'react-native-elements';
+
 export const AppDrawerNavigation = createDrawerNavigator({
     Home : {
       screen : bottomTabNavigator,
+      navigationOptions:{
+        drawerIcon : <Icon name="home" type ="fontawesome5" />
+      }
     },
     MyBarters:{
       screen : MyBartersScreen,
+      navigationOptions:{
+        drawerIcon : <Icon name="gift" type ="font-awesome" />,
+        drawerLabel : "My Received Books"
+      }
     },
     Notifications:{
-      screen: Notifications
+      screen: Notifications,
+      navigationOptions:{
+        drawerIcon : <Icon name="bell" type ="font-awesome" />,
+        drawerLabel : "Notifications"
+      }
     },
     Settings : {
-      screen : SettingsScreen
+      screen : SettingsScreen,
+      navigationOptions:{
+        drawerIcon : <Icon name="settings" type ="font-awesome" />,
+        drawerLabel : "My Received Books"
+      }
     }
     },
     {
